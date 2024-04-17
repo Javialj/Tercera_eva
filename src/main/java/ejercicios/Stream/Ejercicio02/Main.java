066397dea3d205fa408282609847c65d0c2a86fc
Javialj <javialj12@gmail.com>
2024-04-17 18:54:02 +0200

    17/04/2024

--- src/main/java/ejercicios/Stream/Ejercicio02/Main.java
+++ src/main/java/ejercicios/Stream/Ejercicio02/Main.java
@@ -16,8 +16,16 @@ public class Main {
 
         personajes.stream().filter(p -> p.getEdad() < 90 && p.getArma() == Arma.Espada).forEach(p -> System.out.println(p));
         System.out.println("__________________________-");
-        personajes.stream().filter(p -> p.getArma() != Arma.Espada).forEach(p -> System.out.println(p));
+        personajes.stream().filter(p -> !p.getArma().equals(Arma.Espada)).forEach(p -> System.out.println(p));
         System.out.println("__________________________-");
-
+        personajes.stream().sorted((p1, p2) -> p1.getNombre().compareTo(p2.getNombre())).forEach(System.out::println);
+        System.out.println("__________________________-");
+        personajes.stream().filter(p -> p.isHumano()).forEach(System.out::println);
+        System.out.println("__________________________-");
+        personajes.stream().sorted((p1, p2) -> p2.getEdad() - p1.getEdad()).limit(1).forEach(System.out::println);
+        System.out.println("__________________________-");
+        System.out.println("__________________________-");
+        Personaje viejo = personajes.stream().max((p1, p2) -> p1.getEdad() - p2.getEdad()).get();
+        System.out.println(viejo);
     }
 }

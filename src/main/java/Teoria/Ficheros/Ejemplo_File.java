75a731e5b6e3e633bb6a01f36d9cc756d89d8488
Javialj <javialj12@gmail.com>
2024-04-25 16:55:47 +0200

    22/04/2024(24/04/2024)

--- src/main/java/Teoria/Ficheros/Ejemplo_File.java
+++ src/main/java/Teoria/Ficheros/Ejemplo_File.java
@@ -45,5 +45,46 @@ public class Ejemplo_File {
         } else {
             System.out.println("Ni idea papu :v");
         }
+        //Directorio
+
+        try {
+            File directorio = new File(ruta+"carpeta");
+            boolean creado = directorio.mkdir();
+            if (creado) {
+                System.out.println("Ya esta crack, todo en orden.");
+            } else {
+                System.out.println("No se logro :'v ");
+            }
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido una excepción: "+e.getMessage());
+        }
+
+        //Renombrar
+        try {
+            File nuevoNombre = new File(ruta+"Fichero.md");
+            File nombreActual = new File(ruta+"Fichero.txt");
+            boolean renombrado = nombreActual.renameTo(nuevoNombre);
+            if (renombrado) {
+                System.out.println("No le gusto su nombre.");
+            } else {
+                System.out.println("Sigue asi papu :v");
+            }
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido una excepción: "+e.getMessage());
+        }
+
+        //Cmabio de directorio
+        try {
+            File nuevoSitio = new File(ruta + "carpeta\\Fichero.md");
+            File sitioActual = new File(ruta + "Fichero.md");
+            boolean movido = sitioActual.renameTo(nuevoSitio);
+            if (movido) {
+                System.out.println("Se mudo :o");
+            } else {
+                System.out.println("Ni idea pero no pasa na.");
+            }
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido una excepción: "+e.getMessage());
+        }
     }
 }
